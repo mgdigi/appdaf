b9bf908c4fbef1644d0b330652bcc22d7f8c33a2
mgdigi <gueyeprophete287@gmail.com>
2025-07-21 23:28:49 +0000

    ajout du dockerfile

---
+++ src/repository/CitoyenRepository.php
@@ -0,0 +1,15 @@
+<?php 
+namespace App\Repository;
+
+use App\Core\Abstract\AbstractRepository;
+
+class CitoyenRepository extends AbstractRepository{
+    
+   public function selectAll(){}
+     public function insert(array $data){}
+     public function update(){}
+     public function delete(){}
+     public function selectById($id){}
+     public function selectBy(array $filter){}
+
+}
