d27fe04aef48d2ae410d9d3ebcb3c98e33a7c45b
mgdigi <gueyeprophete287@gmail.com>
2025-07-22 00:18:08 +0000

    ajustement du fichier routes

--- app/config/env.php
+++ app/config/env.php
@@ -1,10 +1,6 @@
 <?php
 
-// use Dotenv\Dotenv;
 
-
-// $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
-// $dotenv->load();
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../', null, false);
 $dotenv->safeLoad();
@@ -12,9 +8,7 @@ $dotenv->safeLoad();
 
 define('DB_USER', $_ENV['DB_USER']);
 define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
-define('APP_URL', $_ENV['APP_URL']);
-define('dsn', $_ENV['dsn'] );
 
-define('IMG_DIR' , $_ENV['IMG_DIR']);
+define('dsn', $_ENV['dsn'] );
 
 
